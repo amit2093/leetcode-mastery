23886f623567aec03c0690ba795400b967c7b07c
amit2093 <Arunita@Anita@Sunil@968585>
2026-04-02 21:54:05 +0530

    Problem: 1. Two Sum

---
+++ src/com/amit/arrays/LC1_TwoSum.java
@@ -0,0 +1,45 @@
+package com.amit.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.amit.common.LeetCodeUtils.runTest;
+
+/**
+ * Problem: 1. Two Sum
+ * Difficulty: Easy
+ * Pattern: Hash Map (One-pass)
+ * Time Complexity: O(n) - We traverse the list containing n elements only once.
+ * Space Complexity: O(n) - The extra space required depends on the number of items stored in the hash map.
+ *
+ * Notes: Uses a hash map to store the value and its index. For each element,
+ * we check if its complement (target - current) already exists in the map.
+ */
+public class LC1_TwoSum {
+    public static void main(String[] args) {
+        var sol = new LC1_TwoSum();
+        int[] nums = {3,2,3};
+        int target = 6;
+
+        runTest(
+                new int[]{0,2},
+                () -> sol.twoSum(nums, target)
+        );
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+}
