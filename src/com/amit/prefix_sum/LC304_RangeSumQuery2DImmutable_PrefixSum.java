04640a01a982031ebe47a3bb307b614418870095
amit2093 <Arunita@Anita@Sunil@968585>
2026-04-02 19:53:01 +0530

    Problem: 304. Range Sum Query 2D - Immutable | Prefix Sum | Medium

--- src/com/amit/prefix_sum/LC304_RangeSumQuery2DImmutable_PrefixSum.java
+++ src/com/amit/prefix_sum/LC304_RangeSumQuery2DImmutable_PrefixSum.java
@@ -1,7 +1,5 @@
 package com.amit.prefix_sum;
 
-import static com.amit.common.LeetCodeUtils.runTest;
-
 /**
  * Problem: 304. Range Sum Query 2D - Immutable
  * Difficulty: Medium
@@ -31,21 +29,6 @@ public class LC304_RangeSumQuery2DImmutable_PrefixSum {
                                             - prefixSum[row][col];
             }
         }
-    }
-
-    public static void main(String[] args) {
-        int[][] inputMatrix = {
-                {3, 0, 1, 4, 2},
-                {5, 6, 3, 2, 1},
-                {1, 2, 0, 1, 5},
-                {4, 1, 0, 1, 7},
-                {1, 0, 3, 0, 5}
-        };
-        var sol = new LC304_RangeSumQuery2DImmutable_PrefixSum(inputMatrix);
-        runTest(
-                prefixSum,
-                () -> sol.sumRegion(2, 1, 4, 3)
-        );
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
